4b2097060550555618a372b39ac3fd60d429f854
victorbrunissen <victorbrunissen@gmail.com>
2019-01-11 20:55:29 +0100

    finis les exos ! je passe au dernier jour

---
+++ exo08.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+
+puts "Et ton nom de famille ?"
+print "> "
+user_no = gets.chomp
+
+puts "Bonjour, #{user_name} #{user_no}!"
